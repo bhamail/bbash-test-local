e8455247816bac96e5da84ad60d9788246417795
Dan Rollo <danrollo@gmail.com>
2022-04-05 14:06:10 -0400

    some code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bhamail/bbash-test-local
+
+go 1.17
---
+++ mymain.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Printf("Huzzah, I ran on: %s\n", now.Format(time.RFC3339))
+}
